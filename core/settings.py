cb5caa702825e3aa4b136fe75117bf13f9d3e608
atishraoon <satishoraon881@gmail.com>
2026-02-17 00:47:29 +0530

    update action button

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
- 
+
 ALLOWED_HOSTS = ['satish881.pythonanywhere.com' , '*' , '127.0.0.1' ]  
 
 
