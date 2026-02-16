85294a1d01bad4721c86a3e56bfeeb23b6c841d1
atishraoon <satishoraon881@gmail.com>
2026-02-17 01:03:47 +0530

    vercel deploy settings

--- core/settings.py
+++ core/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['satish881.pythonanywhere.com' , '*' , '127.0.0.1' ]  
+ALLOWED_HOSTS = ['satish881.pythonanywhere.com' , '*' , '127.0.0.1' , '.vercel.app']  
 
-
+ 
 # Application definition
 
 INSTALLED_APPS = [
